ef637ef9fadc4da5a86272563de0b2bf8c95b81a
Aviiiik <avikmallick14@gmail.com>
2024-08-30 23:29:24 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# reachinbox
+ Frontend
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import AllRoutes from './Allroutes/AllRoutes';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>test .....</h1> */}
+      <AllRoutes/>
+    </div>
+  );
+}
+
+// export default App;
